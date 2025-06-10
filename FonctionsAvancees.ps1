55bdf7f861e3ffa730983475b56074a25316e77e
unknown <ihab@utopios.net>
2025-06-10 14:58:01 +0200

    debut de l'utilisation de cmdletBinding

---
+++ FonctionsAvancees.ps1
@@ -0,0 +1,12 @@
+﻿function Nom-Fonction {
+ [CmdletBinding()]
+ param(
+ [string]$Param1,
+ [int]$Param2
+ )
+ # Corps de la fonction
+ Write-Debug "Information debug param 1 est un string et param 2 est int"
+ Write-Output "Param1 = $Param1, Param2 = $Param2"
+ Write-Verbose "Plus d'informations"
+ 
+}Nom-Fonction -Param1 "ttztz" -Param2 10 -Debug
