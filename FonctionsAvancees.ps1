d41121052fb492047306e844038d2ccfaeb08ca1
unknown <ihab@utopios.net>
2025-06-10 15:22:36 +0200

    avec set param

--- FonctionsAvancees.ps1
+++ FonctionsAvancees.ps1
@@ -24,4 +24,24 @@ function Supprimer-Fichier {
 
 # Nom-Fonction -Param1 "ttztz" -Param2 10 -Debug
 
-Supprimer-Fichier -chemin "file.txt" 
+# Supprimer-Fichier -chemin "file.txt" 
+
+function Afficher-Message {
+    [CmdletBinding(DefaultParameterSetName = "FullName")]
+    param(
+        [Parameter(ParameterSetName = "SimpleName", Mandatory = $true)]
+        [string]$Nom,
+        [Parameter(ParameterSetName = "SimpleName", Mandatory = $true)]
+        [string]$Prenom,
+        [Parameter(ParameterSetName = "FullName", Mandatory = $true)]
+        [string]$NomComplet
+    )
+    if ($PSCmdlet.ParameterSetName -eq "SimpleName") {
+        return "$($Nom) $($Prenom)"
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq "FullName") {
+        return $NomComplet
+    }
+}
+
+Afficher-Message -Nom "ABADI" -Prenom "Ihab"
