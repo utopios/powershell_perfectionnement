df2f442f3733d35b1e8eb6dd60a5a6d7a0e597bc
unknown <ihab@utopios.net>
2025-06-10 15:10:14 +0200

    avec confirmImpact

--- FonctionsAvancees.ps1
+++ FonctionsAvancees.ps1
@@ -1,12 +1,27 @@
 ﻿function Nom-Fonction {
- [CmdletBinding()]
- param(
- [string]$Param1,
- [int]$Param2
- )
- # Corps de la fonction
- Write-Debug "Information debug param 1 est un string et param 2 est int"
- Write-Output "Param1 = $Param1, Param2 = $Param2"
- Write-Verbose "Plus d'informations"
+    [CmdletBinding()]
+    param(
+        [string]$Param1,
+        [int]$Param2
+    )
+    # Corps de la fonction
+    Write-Debug "Information debug param 1 est un string et param 2 est int"
+    Write-Output "Param1 = $Param1, Param2 = $Param2"
+    Write-Verbose "Plus d'informations"
  
-}function Supprimer-Fichier {    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='high')]    param(        [string]$chemin    )    if($PSCmdlet.ShouldProcess($chemin, "Suppression")) {        Remove-item $chemin    }}# Nom-Fonction -Param1 "ttztz" -Param2 10 -DebugSupprimer-Fichier -chemin "file.txt" 
+}
+
+function Supprimer-Fichier {
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'high')]
+    param(
+        [string]$chemin
+    )
+    if ($PSCmdlet.ShouldProcess($chemin, "Suppression")) {
+        Remove-item $chemin
+    }
+}
+
+
+# Nom-Fonction -Param1 "ttztz" -Param2 10 -Debug
+
+Supprimer-Fichier -chemin "file.txt" 
