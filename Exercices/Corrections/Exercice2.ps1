f5cb17f27cfc1d8cbcfaff5dcffdad40c36f5412
unknown <ihab@utopios.net>
2025-06-10 11:56:38 +0200

    debut de correction de l'exercice 2

---
+++ Exercices/Corrections/Exercice2.ps1
@@ -0,0 +1,16 @@
+﻿$tasksRecuperees = @()
+$tasks = Get-ScheduledTask | Select-Object -First 10
+$tasks
+foreach($task in $tasks) {
+    $obj = [PSCustomObject]@{
+        Nom = $task.TaskName
+        Chemin = $task.TaskPath
+        Auteur = $task.Author
+        Action = $task.Actions.Execute
+        Statut = (Get-ScheduledTaskInfo -TaskName $task.TaskName).LastTaskResult
+    }
+    $tasksRecuperees += $obj
+}
+
+$tasksRecuperees 
+# $tasksRecuperees | Select-Object Action, Statut
