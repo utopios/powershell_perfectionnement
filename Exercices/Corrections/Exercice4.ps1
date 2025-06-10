d93a82b30c28b2f1e6c074d3c269229f834512ee
unknown <ihab@utopios.net>
2025-06-10 16:06:58 +0200

    debut correction exercice 4

---
+++ Exercices/Corrections/Exercice4.ps1
@@ -0,0 +1,12 @@
+﻿function Manage-UserAccount {
+    [CmdletBinding(DefaultParameterSetName = "Create")]
+    param(
+        [Parameter( Mandatory = $true)]
+        [string]$Username,
+        [Parameter(ParameterSetName = "Create", Mandatory = $true)]
+        [string]$Password,
+        [Parameter(ParameterSetName = "Disable", Mandatory = $true)]
+        [Switch]$Disable
+    )
+    
+}
