f1bfd3476af17b3da8842c283ca23acd54927b75
unknown <ihab@utopios.net>
2025-06-10 16:19:16 +0200

    Correction Exercice 4

--- Exercices/Corrections/Exercice4.ps1
+++ Exercices/Corrections/Exercice4.ps1
@@ -8,5 +8,28 @@
         [Parameter(ParameterSetName = "Disable", Mandatory = $true)]
         [Switch]$Disable
     )
+
+    Write-Verbose "Debut d'execution de la fonction Manage-UserAccount avec $($Username)"
+    Write-Verbose "Jeu de paramètre utilisé $($PSCmdlet.ParameterSetName)"
+
+    switch ($PSCmdlet.ParameterSetName) {
+        'Create' {
+            Write-Verbose "Ajout d'un utilisateur"
+            Write-Output "L'utilisateur $Username a été correctement ajouté"
+        }
+
+        'Disable' {
+            Write-Verbose "désactivation d'un utilisateur"
+            Write-Output "L'utilisateur $Username a été correctement désactivé"
+        }
+
+        default {
+            Write-Error "Erreur de les parameterSetName"
+        }
+    }
     
 }
+
+Manage-UserAccount -Username "Ihab" -Password "123456" -Verbose
+
+Manage-UserAccount -Username "Ihab" -Disable -Verbose
