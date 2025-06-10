bb11f463be3cf614430d798e29952535ac5f6ccc
unknown <ihab@utopios.net>
2025-06-10 10:10:23 +0200

    debut avec support

---
+++ POO.ps1
@@ -0,0 +1,46 @@
+﻿# Méthode 1
+$em = New-Object PSObject
+
+$em | Add-Member -MemberType NoteProperty -Name "Nom" -Value "Abadi"
+$em | Add-Member -MemberType NoteProperty -Name "Prenom" -Value "Ihab"
+
+#$em
+
+# Méthode 2 HashTable
+
+$em2 = [PSCustomObject]@{
+    Nom = "Toto"
+    Prenom= "Titi"
+}
+
+#$em2
+
+# Ajout les types de propriétés
+$produit = [PSCustomObject]@{
+    Nom = "PC"
+    Prix = 10
+    Stock = 100
+}
+
+# Propriété simple
+$produit | Add-Member -MemberType NoteProperty -Name "Categorie" -Value "Informatique"
+# Propriété calculée
+$produit | Add-Member -MemberType ScriptProperty  -Name "PrixTTC" -Value {
+    $this.Prix * 1.20
+}
+# Methode personnalisée
+$produit | Add-Member -MemberType ScriptMethod  -Name "AppliquerRemise" -Value {
+    param([decimal]$pourcentage)
+    $this.Prix = $this.Prix * (1 - $pourcentage / 100)
+    return $this
+}
+
+$produit | Add-Member -MemberType 
+
+$produit.AppliquerRemise(10)
+$produit
+
+
+$p = Get-Process -Name WMIRegistrationService
+$p | Add-Member -MemberType NoteProperty -Name "Commentaire" -Value "Process critique"
+$p.Commentaire
---
+++ serveur.ps1
@@ -0,0 +1,25 @@
+﻿class Serveur {
+ [string]$Nom
+ [string]$IP
+ [string]$Etat
+ [datetime]$DateDernierAudit
+ Serveur([string]$nom, [string]$ip) {
+ $this.Nom = $nom
+ $this.IP = $ip
+ $this.Etat = "Non vérifié"
+ $this.DateDernierAudit = (Get-Date)
+ }
+ Serveur([string]$nom, [string]$ip, [string]$etat) {
+ $this.Nom = $nom
+ $this.IP = $ip
+ $this.Etat = $etat
+ $this.DateDernierAudit = (Get-Date)
+ }
+ [Serveur]AfficherInfos() {
+ Write-Output "Nom=$($this.Nom), IP=$($this.IP), Etat=$($this.Etat), Date=$($this.DateDernierAudit)"
+ return $this
+ }
+} $srv = [Serveur]::new("SRV02", "192.168.1.2", "KO")
+$srv.AfficherInfos()
+$srv.Etat = "OK"
+$srv.AfficherInfos()
