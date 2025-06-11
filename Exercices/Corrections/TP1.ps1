48a8de9065f4a130e2a3ed6ddb248f6f9bf68ff5
unknown <ihab@utopios.net>
2025-06-11 10:24:12 +0200

    debut de correction du TP 1

---
+++ Exercices/Corrections/TP1.ps1
@@ -0,0 +1,46 @@
+function Find-FileAdvanced {
+    [CmdletBinding(DefaultParameterSetName = "ByName", SupportsShouldProcess=$true)]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName=$true)]
+        [string[]] $Path,
+
+        [Parameter(ParameterSetName="ByName", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $Name,
+
+        [Parameter(ParameterSetName="ByName", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string[]] $Extensions,
+        
+        [Parameter(ParameterSetName="ByName", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Switch] $Recurse,
+
+        [Parameter(ParameterSetName="BySize", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [long] $MinSize,
+        
+        [Parameter(ParameterSetName="BySize", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [long] $MaxSize,
+
+        [Parameter(ParameterSetName="BySize", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $SizeUnit,
+
+        [Parameter(ParameterSetName="ByDate", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [datetime] $CreatedAfter,
+
+        [Parameter(ParameterSetName="ByDate", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [datetime] $CreatedBefore,
+
+        [Parameter(ParameterSetName="ByDate", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [datetime] $ModifiedAfter,
+
+        [Parameter(ParameterSetName="ByDate", Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [datetime] $ModifiedBefore,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string[]] $Include,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string[]] $Exclude,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Switch] $Hidden
+    )
+}
